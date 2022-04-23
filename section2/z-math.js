3bb80a467ca2c2f9f52fb566dfe9f33a0aa08c47
Igor Silva <danieligord7@hotmail.com>
2022-04-23 12:52:51 -0300

    Commit M2

---
+++ section2/z-math.js
@@ -0,0 +1,8 @@
+//Usando MATH.ROUND COM RANDOM - Gerando números aleatórios e arredondados
+let num1 = 9.54578
+let aleatorio = Math.random() * (1000 - 1) + 1
+let aleatorioArredondado = Math.round(aleatorio)
+console.log("Aleatoriedade com arredondamento automático: ",aleatorioArredondado)
+
+
+//
