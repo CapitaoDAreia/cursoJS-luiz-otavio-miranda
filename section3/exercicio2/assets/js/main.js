b12868ca60915913cb238990aaab1b0fbe632e7e
Igor Silva <danieligord7@hotmail.com>
2022-05-02 21:01:07 -0300

    Commit M2

--- section3/exercicio2/assets/js/main.js
+++ section3/exercicio2/assets/js/main.js
@@ -8,9 +8,8 @@ let button = document.getElementById('button-submit')
 /*Functions*/
 function calcular(x, y){
     let resultadoIMC = x/((y/100)**2)
-    return resultadoIMC
+    return resultadoIMC.toFixed(2)
 }
-
 
 /*Listeners*/
 button.addEventListener('click', function(){
@@ -20,23 +19,23 @@ button.addEventListener('click', function(){
         alert('Digite os dois valores para continuar!')
     }else{
         if(calcular(peso.value, altura.value) < 18.5){
-            imcResult.innerHTML += `<p>Menor que 18.5</p>`
-            resultado.innerHTML += `<p>Abaixo do peso ideal</p>`
+            imcResult.innerHTML = `<p>${calcular(peso.value, altura.value)}</p>`
+            resultado.innerHTML = `<p>Abaixo do peso ideal</p>`
         }else if(calcular(peso.value, altura.value) >= 18.5 && calcular(peso.value, altura.value) < 24.9){
-            imcResult.innerHTML += `<p>Entre 18.5 e 24.9</p>`
-            resultado.innerHTML += `<p>Normal</p>`
+            imcResult.innerHTML = `<p>${calcular(peso.value, altura.value)}</p>`
+            resultado.innerHTML = `<p>Normal</p>`
         }else if(calcular(peso.value, altura.value) >= 24.9 && calcular(peso.value, altura.value) <= 30){
-            imcResult.innerHTML += `<p>Entre 24.8 e 30</p>`
-            resultado.innerHTML += `<p>Sobrepeso</p>`
+            imcResult.innerHTML = `<p>${calcular(peso.value, altura.value)}</p>`
+            resultado.innerHTML = `<p>Sobrepeso</p>`
         }else if(calcular(peso.value, altura.value) > 30 && calcular(peso.value, altura.value) <= 34.9){
-            imcResult.innerHTML += `<p>Entre 30 e 34.9</p>`
-            resultado.innerHTML += `<p>Obesidade grau 1</p>`
+            imcResult.innerHTML = `<p>${calcular(peso.value, altura.value)}</p>`
+            resultado.innerHTML = `<p>Obesidade grau 1</p>`
         }else if(calcular(peso.value, altura.value) > 35 && calcular(peso.value, altura.value) <= 39.9){
-            imcResult.innerHTML += `<p>Entre 35 e 39.9</p>`
-            resultado.innerHTML += `<p>Obesidade grau 2</p>`
+            imcResult.innerHTML = `<p>${calcular(peso.value, altura.value)}</p>`
+            resultado.innerHTML = `<p>Obesidade grau 2</p>`
         }else if(calcular(peso.value, altura.value) >= 40){
-            imcResult.innerHTML += `<p>Maior ou igual a 40</p>`
-            resultado.innerHTML += `<p>Obesidade grau 3</p>`
+            imcResult.innerHTML = `<p>${calcular(peso.value, altura.value)}</p>`
+            resultado.innerHTML = `<p>Obesidade grau 3</p>`
         }else{
             alert('Algo deu errado, revise os dados e tente novamente.')
         }
