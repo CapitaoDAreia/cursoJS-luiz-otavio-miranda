8919fef966e1655c857bde1c86c8b4e2a7de0bda
Igor Silva <danieligord7@hotmail.com>
2022-05-22 11:16:32 -0300

    Alguns testes baseados na section 3

--- ww-testes/captura teclas/captura-teclas.js
+++ ww-testes/captura teclas/captura-teclas.js
@@ -1,4 +1,12 @@
 const input = document.querySelector('#button');
 const paragraph = document.querySelector('.container-element');
 
-input.addEventListener('click', )
+input.addEventListener('keypress', function(e){
+    const p = document.createElement('p')
+    p.innerHTML =   ` Você teclou ${e.code}. </br>
+                      O código da tecla é ${e.charCode}.
+                    `
+    paragraph.appendChild(p)
+    console.log(e)
+
+})
