4946348a422e64425af657d4c0c7e87ecae32cef
Igor Silva <danieligord7@hotmail.com>
2022-05-24 21:27:15 -0300

    Testando funcionalidades e manipulando a DOM: Testes com criação de elementos, adição de elementos no documento, exclusão de elementos e seletores novos como e.target, parentElement e a função .remove() além de salvamento de informações no client-side com JSON

--- ww-testes/diversosTestes/criarElementos.js
+++ ww-testes/diversosTestes/criarElementos.js
@@ -1,6 +1,7 @@
 const textinput = document.querySelector('#textinput');
 const button = document.querySelector('input[type="button"]');
 const containerNovosElementos = document.querySelector('.container-novos-elementos');
+const saveButton = document.querySelector('#salvar')
 
 /*
     FUNÇÕES
@@ -32,7 +33,27 @@ function adicionaElementosNoContainer(elemento) {
     containerNovosElementos.appendChild(elemento)
 }
 
+//Salvar meus itens
+function salvarElementos(){
+    const listaComElementosDoContainer = containerNovosElementos.querySelectorAll('p');
+    let elementosSalvos = []
+    for(let item of listaComElementosDoContainer){
+        elementosSalvos.push(item.innerText.replace('Excluir', ''))
+    }
+    const elementosSalvosJSON = JSON.stringify(elementosSalvos)
+    localStorage.setItem('itens', elementosSalvosJSON)
+}
 
+//Buscar itens salvos
+function buscarItensSalvos(){
+    const itensBuscados = localStorage.getItem('itens');
+    const itensBuscadosArray = JSON.parse(itensBuscados);
+
+    for(let item of itensBuscadosArray){
+        let novoElemento = criarElementos('p', item);
+        adicionaElementosNoContainer(novoElemento)
+    }
+}
 
 /*
     LISTENERS
@@ -42,12 +63,26 @@ button.addEventListener('click', function(){
     let novoElemento = criarElementos('p', textinput.value);
     adicionaElementosNoContainer(novoElemento)
     textinput.focus()//Focar no textinput após clicar
+    textinput.value = "";
 })
+
 //Excluindo o elemento com seletores específicos
 document.addEventListener('click', function(e){
-    console.log(e.target.innerText)
     if(e.target.innerText === "Excluir"){
         e.target.parentElement.remove()
     }
+})
 
+//Salvando itens localmente com a função salvar elementos
+saveButton.addEventListener('click', function(){
+    if(containerNovosElementos.childElementCount != 0){
+        salvarElementos()
+        alert('Itens salvos localmente!')
+    }else{
+        alert('Lista salva sem itens')
+        salvarElementos()
+    }
+})
+window.addEventListener('load', function(){
+    buscarItensSalvos()
 })
---
+++ ww-testes/diversosTestes/diversosTestes.md
@@ -0,0 +1,2 @@
+# Diversos Testes 
+- Aqui eu brinco um pouco com manipulação da DOM. Criando elementos apartir de um input, excluindo esses elementos e salvando eles localmente utilizando JSON.
