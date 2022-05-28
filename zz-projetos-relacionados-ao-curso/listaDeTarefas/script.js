677b9d9c3671965b1ef57bd33ca4109615c0184c
Igor Silva <danieligord7@hotmail.com>
2022-05-28 13:16:24 -0300

    Desenvolvimento do projeto LISTA DE TAREFAS, fases finais. Falta coletar itens salvos localmente quando o usuário reabrir a página.

--- zz-projetos-relacionados-ao-curso/listaDeTarefas/script.js
+++ zz-projetos-relacionados-ao-curso/listaDeTarefas/script.js
@@ -5,6 +5,8 @@ const time = document.querySelector('.time');
 const text = document.querySelector('#text');
 const button = document.querySelector('.addtask');
 const container = document.querySelector('.container');
+const windowAlert = document.querySelector('.window-alert');
+const arrayWithTasks = [];
 
 /*
     FUNCTIONS
@@ -19,23 +21,66 @@ setInterval(function(){
     time.innerHTML = realDate;
 }, 1000)
 
-//
-function createElement(element, content){
-    const newElement = document.createElement(element)
+//Create an element
+function createElement(element, content, myClass){
+    const newElement = document.createElement(element);
+    newElement.classList.add(myClass);
     newElement.innerHTML = content;
     return newElement
 }
+//Add element into container
 function addElement(newElement){
     container.appendChild(newElement)
+}
+//Runs windowAlert
+function alertErrorWindow(myClass, myContent){
+    windowAlert.classList.add(myClass)
+    windowAlert.innerHTML = myContent;
+    setTimeout(function(){
+        windowAlert.classList.remove(myClass)
+    }, 3000)
+}
+//Save tasks in an array
+function saveTasks(){
+    const currentTasks = container.querySelectorAll('p');
+    for(let tasks of currentTasks){
+        arrayWithTasks.push(tasks.innerText)
+    }
+}
+//Save tasks in local storage with an JSON
+function saveTasksInLocal(){
+    const savedData = JSON.stringify(arrayWithTasks);
+    localStorage.setItem('savedData', savedData);
 }
 
 
 
 /*
-    LISTENERS
+LISTENERS
 */
+//Add a new task
 button.addEventListener('click', function(){
-    if(!text.value)alert('Vazio')
-    addElement(createElement('p',text.value))
+    if(!text.value){
+        alertErrorWindow('window-alert-dinamic', 'Add what?')
+    }else{
+        addElement(createElement('p',text.value))
+        addElement(createElement('button', 'Delete', 'delete-button'))
+        text.value = ""
+        if(window.innerWidth > 400){
+            text.focus()
+        }
+    }
 })
+//Remove previous added task
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('delete-button')){
+        e.target.previousSibling.remove()
+        e.target.remove()
+    }
+})
+//Save tasks when user closes the page
+window.onbeforeunload = function(){
+    saveTasks()
+    saveTasksInLocal()
+}
 
