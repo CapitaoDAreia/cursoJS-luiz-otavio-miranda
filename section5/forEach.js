81bf6463086860a9d8482d1f912daa74b105e285
Igor Silva <danieligord7@hotmail.com>
2022-06-16 11:09:16 -0300

    Section 5: Tratando arrays com reduce, map e filter & método forEach

---
+++ section5/forEach.js
@@ -0,0 +1,10 @@
+/*
+    FOR EACH
+    Um método que incide sobre arrays, recebe uma função que, automaticamente recebe três parâmetros.
+*/
+
+const numeros = [5, 50, 80, 1,];
+
+numeros.forEach((valor, indice, array) =>{
+    console.log(valor, indice, array)
+})
