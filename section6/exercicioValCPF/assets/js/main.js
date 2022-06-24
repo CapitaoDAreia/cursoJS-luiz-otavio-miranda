308989d830451f2b2d3ecf46ee4e8e552462d898
Igor Silva <danieligord7@hotmail.com>
2022-06-24 19:24:57 -0300

    Section 6: Finalizando exercício validação de CPFs

--- section6/exercicioValCPF/assets/js/main.js
+++ section6/exercicioValCPF/assets/js/main.js
@@ -4,26 +4,73 @@
 */
 const cpfNumber = document.querySelector('#cpf');
 const buttonCheck = document.querySelector('button[type="submit"]');
+const popupWindow = document.querySelector('.popup');
 
 /*
     FUNCTIONS
 
 */
-    function removeLetras(){
-        const valor = cpfNumber.value;
-        cpfNumber.value = valor.replace(/\D+/g, '');
+
+    function calculador(begin, array){
+        let counter = begin;
+        let total = 0;
+        for(let i of array){
+            total += counter*i;
+            counter--;
+        }
+        return 11-(total%11)
+    }
+    function transformaEmArray(){
+        let valor = Array.from(cpfNumber.value);
+        return valor;
+    }
+    function retornaPrimeiroDigito(){
+        let valor = transformaEmArray();
+        valor.splice(-2, 2);
+        let resultado = calculador(10, valor);
+        return resultado <= 9? resultado : 0;
     }
 
-    function validaCPF(){
-        if(cpfNumber.value.length > 11 || cpfNumber.value.length < 11){
-            alert('Número incorreto de caracteres.')
-        }
+    function retornaSegundoDigito(){
+        let valor = transformaEmArray();
+        valor.splice(-1, 1);
+        let resultado = calculador(11, valor);
+        return resultado <= 9? resultado : 0;
     }
+
+    function concatenaDigitos(){
+        const primeiroDigito = String(retornaPrimeiroDigito());
+        const segundoDigito = String(retornaSegundoDigito());
+        let cpfMod = transformaEmArray();
+        cpfMod.splice(-2, 2)
+        cpfMod.push(primeiroDigito, segundoDigito)
+        return cpfMod;
+    }
+    function popup(text, attribute){
+        popupWindow.classList.add('dinamic-popup')
+        popupWindow.setAttribute('style', attribute)
+        popupWindow.innerText = text;
+        setTimeout(()=>{
+            popupWindow.classList.remove('dinamic-popup')
+        }, 3000)
+    }
+    function validaCPF(){
+        let cpfMod = String(concatenaDigitos());
+        let cpfOriginal = String(transformaEmArray());
+        if(cpfMod === cpfOriginal){
+            popup('CPF Válido', 'background-color: green;')
+        }else{
+            popup('CPF Inválido', 'background-color: red;')
+        }
+        return cpfMod === cpfOriginal;
+    }
+
 /*
     LISTENERS
 
 */
-buttonCheck.addEventListener('click', function(){
+buttonCheck.addEventListener('click', function(e){
+    e.preventDefault()
     validaCPF()
 })
 
