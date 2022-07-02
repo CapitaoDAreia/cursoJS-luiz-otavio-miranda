698615abdcdfbef0913bb85ccf451a534b6f0565
Igor Silva <danieligord7@hotmail.com>
2022-07-02 14:47:33 -0300

    Section 7: Validação de formulário - Necessário validar length da senha

---
+++ zz-projetos-relacionados-ao-curso/validandoForm/js/main.js
@@ -0,0 +1,53 @@
+const buttonSubmit = document.querySelector('.container-form > button[type="submit"]');
+
+function verificaVazio(){
+    let vazios = false;
+    const form = document.querySelectorAll('.container-form > div > input');
+    for(let campo of form){
+        if(campo.value === ""){
+            campo.nextElementSibling.classList.add('error')
+            vazios = true;
+            return vazios;
+        }else{
+            campo.nextElementSibling.classList.remove('error')
+            vazios = false;
+        }
+    }
+    return vazios;
+}
+
+function verificaUsuario(){
+    const campoUsuario = document.querySelector('#usuarioInput');
+    let re = /^[a-zA-Z\-\ \s\ 0-9]+$/;
+    if(!re.test(campoUsuario.value)){
+        campoUsuario.nextElementSibling.innerText = "O campo USUARIO deve conter letras ou números."
+        campoUsuario.nextElementSibling.classList.add('error')
+        return false;
+    }else{
+        campoUsuario.nextElementSibling.classList.remove('error')
+        return true;
+    }
+}
+
+function verificaSenha(){
+    const campoSenha = document.querySelector('#senhaInput');
+    const campoRepeteSenha = document.querySelector('#rSenhaInput');
+    if(campoSenha.value !== campoRepeteSenha.value ){
+        campoRepeteSenha.nextElementSibling.innerText = "As senhas não coincidem."
+        campoRepeteSenha.nextElementSibling.classList.add('error')
+        return false;
+    }else{
+        campoRepeteSenha.nextElementSibling.classList.remove('error')
+        return true;
+    }
+}
+
+
+
+buttonSubmit.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(verificaVazio() !== false) return;
+    if(verificaCPF() === false) return;
+    if(verificaUsuario() !== true) return;
+    if(verificaSenha() !== true) return;
+})
