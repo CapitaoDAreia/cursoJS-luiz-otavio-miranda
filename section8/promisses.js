0af735dd37a4b5a4c46a2a6b7e7f1a01f63a8979
Igor Silva <danieligord7@hotmail.com>
2022-07-27 21:45:42 -0300

    Section 8: Começando a entender promisses, entendendo alguns conceitos.

---
+++ section8/promisses.js
@@ -0,0 +1,11 @@
+/*
+        PROMISSES
+*/
+
+/*
+    Uma Promisse é um objeto utilizado para processamento assíncrono. Significa exatamente
+    promessa. É um objeto que representa a eventual conclusão ou falha de uma operação assíncrona. 
+    É um obeto retornado para o qual você adiciona callbacks, em vez de passar callbacks para uma função
+
+    ** PRECISO ESTUDAR MUITO MAIS SOBRE ESTE TEMA, INFELIZMENTE POR HOJE SERÁ SOMENTE ISSO. **
+*/
