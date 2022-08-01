41c7806e2ff11aaf7ba1f8c5f3112353067d2c70
Igor Silva <danieligord7@hotmail.com>
2022-08-01 09:31:40 -0300

    Section 8: Explorando métodos úteis para promises

--- section8/metodos-uteis-promises.js
+++ section8/metodos-uteis-promises.js
@@ -1,3 +1,54 @@
 /*
     Explorando métodos úteis para promises
 */
+/*
+    Promise.all - Tenta resolver todas promises passadas em um array e devolve um array com a resolução. Se uma delas cair em catch, retorna somente o erro.
+    Promise.race - Retorna sempre a promise que se resolver/der erro primeiro.
+    romise.resolve 
+    Promise.reject
+*/
+function rand(min, max){
+    min*=1000;
+    max*=1000;
+    return Math.floor(Math.random()*(max-min)+min);
+}
+function geraRetorno(msg, tempo){
+    return new Promise((resolve, reject) => {
+        
+        setTimeout(() => {
+            if(typeof msg !== 'string') {
+                reject('Cai no erro');
+                return;
+            } 
+
+            resolve(msg);
+
+        }, tempo);
+    })
+}
+
+//Promise.all
+const promises = [
+    // 'Primeiro valor',
+    geraRetorno('Promise 1', rand(1, 5)),
+    geraRetorno('Promise 2', rand(1, 5)),
+    geraRetorno('Promise 3', rand(1, 5)),
+    geraRetorno(10, rand(1, 5)),
+    // 'Outro valor'
+];
+
+Promise.all(promises)
+    .then(valor=>{
+        console.log("All: "+valor);
+    }).catch(erro=>{
+        console.log("All: "+erro);
+    });
+
+//Promise.race
+Promise.race(promises).then(valor=>{
+    console.log("Race: "+valor);
+}).catch(erro=>{
+    console.log("Race: "+erro);
+});
+
+//
